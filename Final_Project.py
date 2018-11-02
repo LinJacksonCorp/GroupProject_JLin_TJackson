3d8399a13d9db03aaaf92014bbc41f7226e73d7b
Justin Lin <jlin21@choate.edu>
2018-11-02 11:11:34 -0400

    .

---
+++ Final_Project.py
@@ -0,0 +1,10 @@
+# Name: Justin Lin
+# Date: 11/2/2018
+# Description: Final Project ----> Periodic Table Element Table
+# Source(s): 
+
+
+class Periodic_data():
+	def __init__(self):
+		self.data
+		self.
