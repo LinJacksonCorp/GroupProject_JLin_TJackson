ba3ae2bb2fdfd464e1f791dabfe0cc06e1765de2
tjackson21 <43040910+tjackson21@users.noreply.github.com>
2018-11-05 12:14:21 -0500

    Class Work

--- GroupFinishThatHeck.py
+++ GroupFinishThatHeck.py
@@ -1,4 +1,7 @@
-#Group Project
+# Name: Justin Lin & Tilden Jackson
+# Date: 11/2/2018
+# Description: Final Project ----> Periodic Table Element Table
+# Source(s): (https://realpython.com/python-csv/), (https://www.youtube.com/watch?v=q5uM4VKywbA)
 import csv
 
 class PeriodicTable:
@@ -7,14 +10,14 @@ class PeriodicTable:
 		# with open('elements.csv') as csv_file:
 		# csv_reader = csv.reader(csv_file, delimiter=',')
 	def Mass(self, str_compound):
-		compound = []
+		compound = [] 
 		for a in str_compound:
-			compound += [a]
-		compound+=[0]
-		i = 0
-		while i<len(str_compound)+1:
-			if str(compound[i]).isdigit() == True:
-				e = 1
+			compound += [a] # this will store all the characters & digits mentioned in the d.Mass(___) below
+		compound+=[0] # this exists to let the code read the end of the string
+		i = 0 # The limiter of the while loop below
+		while i<len(str_compound)+1: # This will loop through the enitrety of the compound string
+			if str(compound[i]).isdigit() == True: # This checks to see if the compound is a digit. 
+				e = 1 # This variable will move the checking mechanism for the integers inside 
 				if compound[i] == 0 and i == len(str_compound) and str(compound[i-1]).isdigit() == False:
 					n = 1
 				else:
@@ -31,7 +34,7 @@ class PeriodicTable:
 						n += str(compound[i+r])
 				self.elements += [element]*int(n)
 				i+=(e-1)
-			elif compound[i] == compound[i].upper():
+			elif compound[i] == compound[i].upper(): # this will run through when it sees a capilized 
 				element = str(compound[i])
 				if str(compound[i+1]).isdigit() == False:
 					if compound[i+1] == compound[i+1].upper():
@@ -41,8 +44,9 @@ class PeriodicTable:
 				if str(compound[i+1]).isdigit() == False:
 					self.elements += [element]
 			i+=1
-		# print(self.elements)
+		print(self.elements)
 		#add up values by taking value from csv. Then return val
+
 	def MolarMass(self, elsymb):
 		import os, csv
 		os.chdir(r'C:\Users\tilde\Desktop')
@@ -53,6 +57,7 @@ class PeriodicTable:
 				if str(elsymb) == str(line[2]):
 					return float(line[3])
 
+
 d = PeriodicTable()
-# d.Mass('H2O')
-print(d.MolarMass('Si'))
+d.Mass('H2O')
+
